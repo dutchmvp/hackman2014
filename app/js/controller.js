5afa5c20d2a5961b2b194b74f99ee519122e9cf3
Jason Staerck <jasonstaerck@gmail.com>
2014-10-25 18:48:26 +0100

    mid leaderboard commit

--- app/js/controller.js
+++ app/js/controller.js
@@ -3,7 +3,62 @@ angular.module('FSUGame.controllers', [])
     .controller('ctrlApp', ['$scope', function ($scope) {
 		console.log('main controller');
         
-        $scope.gameState = 0;
+        $scope.gameState = 2;
+        $scope.gamedevice = 'desktop';
+        
+        $scope.players = {
+            1 : {
+                name:'jason',
+                avatar: 'https://cdn2.iconfinder.com/data/icons/faceavatars/PNG/D04.png',
+                score: 10
+            },
+            2 : {
+                name:'Matt',
+                avatar: 'https://cdn2.iconfinder.com/data/icons/faceavatars/PNG/D04.png',
+                score: 2
+            },
+            3 : {
+                name:'jason',
+                avatar: 'https://cdn2.iconfinder.com/data/icons/faceavatars/PNG/D04.png',
+                score: 10
+            },
+            4 : {
+                name:'jason',
+                avatar: 'https://cdn2.iconfinder.com/data/icons/faceavatars/PNG/D04.png',
+                score: 10
+            },
+            5 : {
+                name:'jason',
+                avatar: 'https://cdn2.iconfinder.com/data/icons/faceavatars/PNG/D04.png',
+                score: 10
+            },
+            6 : {
+                name:'jason',
+                avatar: 'https://cdn2.iconfinder.com/data/icons/faceavatars/PNG/D04.png',
+                score: 10
+            },
+            7 : {
+                name:'jason',
+                avatar: 'https://cdn2.iconfinder.com/data/icons/faceavatars/PNG/D04.png',
+                score: 10
+            },
+            8 : {
+                name:'jason',
+                avatar: 'https://cdn2.iconfinder.com/data/icons/faceavatars/PNG/D04.png',
+                score: 10
+            },
+            9 : {
+                name:'jason',
+                avatar: 'https://cdn2.iconfinder.com/data/icons/faceavatars/PNG/D04.png',
+                score: 10
+            },
+            10 : {
+                name:'jason',
+                avatar: 'https://cdn2.iconfinder.com/data/icons/faceavatars/PNG/D04.png',
+                score: 10
+            }
+        
+        }
         
     }])
     .controller('ctrlHome', ['$scope', function ($scope) {
